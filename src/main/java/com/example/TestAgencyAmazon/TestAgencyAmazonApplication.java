c363f1a72b60963af981348b86152db79f3374f5
Petro Smoliar <143265445+Petro-Smoliar@users.noreply.github.com>
2024-10-24 14:33:12 +0200

    Init Spring

---
+++ src/main/java/com/example/TestAgencyAmazon/TestAgencyAmazonApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestAgencyAmazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAgencyAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAgencyAmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestAgencyAmazon/TestAgencyAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestAgencyAmazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAgencyAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
